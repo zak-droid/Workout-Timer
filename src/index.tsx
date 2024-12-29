2637a843ef63d1737840919c51f3bc2b41b01380
zak-droid <zakbennett@gmail.com>
2024-12-29 13:55:28 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import WorkoutTimer from "./components/ui/WorkoutTimer";
+import WorkoutTimer from "./WorkoutTimer";
 
 ReactDOM.render(
   <React.StrictMode>
